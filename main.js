f76aa645a9f9e039960252189cccf2b45396e669
Azamat <azamurat0@gmail.com>
2023-03-08 00:24:45 +0500

    v1

---
+++ main.js
@@ -0,0 +1,34 @@
+var word = prompt('укажите свое имя ')
+console.log('меня зовут' , word );
+
+var old = +prompt('укажите свои возраст')
+console.log('мне' , old);
+
+var num1 = +prompt('Пример 1: 25-5=?')
+alert(num1);
+console.log('Пример 1: 25-5=' + '20');
+
+var num2 = +prompt('Пример 2: 25+5=?')
+alert(num2);
+console.log('Пример 2: 25+5=' + '30');
+
+var num3 = +prompt('Пример 3: 25:5=?')
+alert(num3);
+console.log('Пример 3: 25:5=' + '5');
+
+var num4 = +prompt('Пример 4: 25*5=?')
+alert(num4);
+console.log('Пример 4: 25*5=' + '125');
+
+var num5 = +prompt('Пример 5: 27%5=?')
+alert(num5);
+console.log('Пример 5: 27%5=' + '2');
+
+var answer = alert('Откройте ' + 'консоль ' + 'для ' + 'просмотра ');
+
+
+var x = +prompt("введите любое число");
+var y = +prompt("введите любое число");
+var z = +prompt("введите любое число");
+var res = (x+z+y) / 3;
+alert("Среднее арефмитическое число это: " + res);
